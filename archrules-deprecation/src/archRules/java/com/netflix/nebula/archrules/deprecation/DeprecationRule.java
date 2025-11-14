4ceeeba36f42f72fe4d8232c4ab2494feb984f0d
Emily Yuan <eyuan@netflix.com>
2025-11-14 15:23:31 -0800

    Add kotlin deprecation annotations to deprecation rule

--- archrules-deprecation/src/archRules/java/com/netflix/nebula/archrules/deprecation/DeprecationRule.java
+++ archrules-deprecation/src/archRules/java/com/netflix/nebula/archrules/deprecation/DeprecationRule.java
@@ -17,14 +17,28 @@ public class DeprecationRule implements ArchRulesService {
     /**
      * This rule is a stop-gap to find all deprecations. This is likely very noisy, so is a Low priority.
      * It is recommended to craft more targeted deprecation rules when targeting actual removal of deprecated code.
+     *
+     * This rule catches:
+     * - Java @Deprecated annotations
+     * - Kotlin @Deprecated annotations
+     * - Kotlin @DeprecatedSinceKotlin annotations
      */
     public static ArchRule deprecationRule = ArchRuleDefinition.priority(Priority.LOW)
             .noClasses()
+            // Java deprecated
             .should().dependOnClassesThat().areAnnotatedWith(Deprecated.class)
             .orShould().accessTargetWhere(targetOwner(annotatedWith(Deprecated.class)))
             .orShould().accessTargetWhere(target(annotatedWith(Deprecated.class)))
+            // Kotlin deprecated
+            .orShould().dependOnClassesThat().areAnnotatedWith("kotlin.Deprecated")
+            .orShould().accessTargetWhere(targetOwner(annotatedWith("kotlin.Deprecated")))
+            .orShould().accessTargetWhere(target(annotatedWith("kotlin.Deprecated")))
+            // Kotlin DeprecatedSinceKotlin
+            .orShould().dependOnClassesThat().areAnnotatedWith("kotlin.DeprecatedSinceKotlin")
+            .orShould().accessTargetWhere(targetOwner(annotatedWith("kotlin.DeprecatedSinceKotlin")))
+            .orShould().accessTargetWhere(target(annotatedWith("kotlin.DeprecatedSinceKotlin")))
             .allowEmptyShould(true)
-            .as("No code should reference deprecated APIs")
+            .as("No code should reference deprecated APIs (Java or Kotlin)")
             .because("usage of deprecated APIs introduces risk that future upgrades and migrations will be blocked");
 
     @Override
---
+++ archrules-deprecation/src/archRulesTest/java/com/netflix/nebula/archrules/deprecation/KotlinDeprecationRuleTest.java
@@ -0,0 +1,50 @@
+package com.netflix.nebula.archrules.deprecation;
+
+import com.netflix.nebula.archrules.core.Runner;
+import com.tngtech.archunit.lang.EvaluationResult;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class KotlinDeprecationRuleTest {
+    private static final Logger LOG = LoggerFactory.getLogger(DeprecationRuleTest.class);
+
+    @Test
+    public void test_kotlin_deprecated_class() {
+        final EvaluationResult result = Runner.check(DeprecationRule.deprecationRule, KotlinDeprecationRuleTest.CodeThatUsesKotlinDeprecatedClass.class);
+        LOG.info(result.getFailureReport().toString());
+        assertThat(result.hasViolation()).isTrue();
+    }
+
+    @Test
+    public void test_deprecated_since_kotlin_class() {
+        final EvaluationResult result = Runner.check(DeprecationRule.deprecationRule, KotlinDeprecationRuleTest.CodeThatUsesDeprecatedSinceKotlinClass.class);
+        LOG.info(result.getFailureReport().toString());
+        assertThat(result.hasViolation()).isTrue();
+    }
+
+    // test helper classes (kotlin deprecated)
+    @kotlin.Deprecated(message="deprecated")
+    static class KotlinDeprecatedClass {
+        static void method() { }
+    }
+
+    @kotlin.DeprecatedSinceKotlin
+    static class DeprecatedSinceKotlinClass {
+        static void method() { }
+    }
+
+    static class CodeThatUsesKotlinDeprecatedClass {
+        static void usage() {
+            KotlinDeprecatedClass.method();
+        }
+    }
+
+    static class CodeThatUsesDeprecatedSinceKotlinClass {
+        static void usage() {
+            DeprecatedSinceKotlinClass.method();
+        }
+    }
+}
