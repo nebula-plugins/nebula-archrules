35608ca369702dd43200f90eb6c61050c40876de
John Burns <wakingrufus@gmail.com>
2026-01-21 17:07:08 -0600

    update archrules plugin to produce rules libraries with new usage attribute

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,3 +15,72 @@ contacts {
         github = "nebula-plugins"
     }
 }
+
+// the following is to avoid project dependency substitutions in archrules runtimes (which breaks lock state)
+subprojects {
+    plugins.withId("com.netflix.nebula.archrules.runner") {
+        configurations.named("testArchRulesRuntime").configure {
+            resolutionStrategy.dependencySubstitution {
+                // workaround for classpath issue
+                all {
+                    val requestedProject = requested
+                    if (requestedProject is ProjectComponentSelector) {
+                        useTarget("com.netflix.nebula" + requestedProject.projectPath + ":latest.release")
+                    }
+                }
+            }
+        }
+        configurations.named("mainArchRulesRuntime").configure {
+            resolutionStrategy.dependencySubstitution {
+                // workaround for classpath issue
+                all {
+                    val requestedProject = requested
+                    if (requestedProject is ProjectComponentSelector) {
+                        useTarget("com.netflix.nebula" + requestedProject.projectPath + ":latest.release")
+                    }
+                }
+            }
+        }
+        plugins.withId("com.netflix.nebula.archrules.library") {
+            configurations.named("archRulesArchRulesRuntime").configure {
+                resolutionStrategy.dependencySubstitution {
+                    // workaround for classpath issue
+                    all {
+                        val requestedProject = requested
+                        if (requestedProject is ProjectComponentSelector) {
+                            useTarget("com.netflix.nebula" + requestedProject.projectPath + ":latest.release")
+                        }
+                    }
+                }
+            }
+            configurations.named("archRulesTestArchRulesRuntime").configure {
+                resolutionStrategy.dependencySubstitution {
+                    // workaround for classpath issue
+                    all {
+                        val requestedProject = requested
+                        if (requestedProject is ProjectComponentSelector) {
+                            useTarget("com.netflix.nebula" + requestedProject.projectPath + ":latest.release")
+                        }
+                    }
+                }
+            }
+
+            // avoid self-referencing dependencies
+            configurations.named("archRulesTestArchRulesRuntime"){
+                resolutionStrategy {
+                    exclude(module = this@subprojects.name)
+                }
+            }
+            configurations.named("archRulesTestRuntimeClasspath"){
+                resolutionStrategy {
+                    exclude(module = this@subprojects.name)
+                }
+            }
+            configurations.named("archRulesTestCompileClasspath"){
+                resolutionStrategy {
+                    exclude(module = this@subprojects.name)
+                }
+            }
+        }
+    }
+}
